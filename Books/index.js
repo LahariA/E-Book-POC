ea8b76be102071c31b8058e0a90ca390839289c5
Lahari Addasarigari <alahari@corp.ojas-it.com>
2020-10-01 18:50:49 +0530

    updated on 01-10-2020

--- Books/index.js
+++ Books/index.js
@@ -1,17 +1,15 @@
-
-
 let ListOfFilterData = "";
 let objectOfOrders = [];
 let valueOfInput = "";
 let allItems = [];
 let totalPrice = 0;
-let serachCount =0;
+let serachCount = 0;
 let count = 0;
 let filObj = [];
-
-function disableOrderButton(){
-document.getElementById('orderId').disabled = true; 
-document.getElementById('orderId').title = "please select atleast one record"; 
+let totalList = [];
+function disableOrderButton() {
+  document.getElementById('orderId').disabled = true;
+  document.getElementById('orderId').title = "please select atleast one record";
 }
 disableOrderButton();
 function display() {
@@ -21,48 +19,48 @@ function display() {
   if (ListOfFilterData.length == 0) {
     x.style.display = "none";
     y.style.display = "none";
-    
+
   } else {
     x.style.display = "block";
-  
+
   }
-  if(allItems.length == 0){
+  if (allItems.length == 0) {
     y.style.display = "none";
   }
   else {
     y.style.display = "block";
-  
+
   }
 }
+
 display();
+function disable(val) {
 
-function disable(val){
+  if (val) {
+    document.getElementById('searchId').disabled = false;
+    document.getElementById('searchId').title = '';
+  } else {
+    document.getElementById('searchId').disabled = true;
+    document.getElementById('searchId').title = 'please select atleast one';
+  }
 
-if(val) { 
-  document.getElementById('searchId').disabled = false; 
-  document.getElementById('searchId').title = ''; 
-} else { 
-  document.getElementById('searchId').disabled = true;
-  document.getElementById('searchId').title = 'please select atleast one'; 
+
 }
 
 
-}
 
-
-
-function disableOrder(val){
-console.log(val,"checked or not");
-  if(val.checked ==  true) { 
-    document.getElementById('orderId').disabled = false; 
-    document.getElementById('orderId').title = ''; 
-  } else { 
+function disableOrder(val) {
+  console.log(val, "checked or not");
+  if (val.checked == true) {
+    document.getElementById('orderId').disabled = false;
+    document.getElementById('orderId').title = '';
+  } else {
     document.getElementById('orderId').disabled = true;
-    document.getElementById('orderId').title = 'please select atleast one record'; 
+    document.getElementById('orderId').title = 'please select atleast one record';
   }
-  
-  
-  }
+
+
+}
 
 
 disable();
@@ -84,18 +82,15 @@ getBooks();
 function get() {
 
   var table = document.getElementById('tableId');
-  let totalList = ListOfFilterData;
-if(totalList.length == 0){
-   var x = document.getElementById("snackbar");
+  totalList = ListOfFilterData;
+  if (totalList.length == 0) {
+    var x = document.getElementById("snackbar");
     x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+  }
   for (const list of totalList) {
 
-    // let tablerow1 = document.getElementById(list.bookName);
-
-    //  console.log(tablerow1.id ,"tablerow");
-    //  if(tablerow1.id == list.bookName )
+   
     let tablerow = document.createElement('tr');
     let tableDataCkeck = document.createElement('td');
     let tableData1 = document.createElement('td');
@@ -107,8 +102,7 @@ if(totalList.length == 0){
     text.setAttribute('type', 'checkbox');
     text.setAttribute('id', list.bookName);
     text.setAttribute('value', list.bookName);
-  //  let inputCheck= document.getElementById(list.bookName)
-    text.setAttribute("onchange","disableOrder(this)");
+    text.setAttribute("onchange", "disableOrder(this)");
 
     text.setAttribute('name', 'book');
     let text1 = document.createTextNode(list.authorName);
@@ -117,8 +111,7 @@ if(totalList.length == 0){
     let text3 = document.createTextNode(list.price);
     let text4 = document.createElement('input');
     text4.setAttribute('id', list.qId);
-    // text.setAttribute('value',);
-    //  valueOfInput =  text4.value;
+   
 
     tableDataCkeck.appendChild(text);
     tableData1.appendChild(text1);
@@ -143,12 +136,11 @@ if(totalList.length == 0){
     allItems.push(obj);
 
     valueOfInput = document.getElementById(list.bookName);
-    console.log(valueOfInput.checked ,"valueOfInput");
-    // disableOrder(this.value){
-
-    // }
+    console.log(valueOfInput.checked, "valueOfInput");
+  
 
   }
+ 
   console.log(totalList, "unnava");
 
 
@@ -170,13 +162,9 @@ function getCustomerData() {
       tablerow.id = receivedList.authorName;
 
       let tableData1 = document.createElement('td');
-      // tableData1.id = receivedList.authorName;
       let tableData2 = document.createElement('td');
-      // tableData1.id = receivedList.bookName;
       let tableData3 = document.createElement('td');
-      // tableData1.id = receivedList.authodId;
       let tableData4 = document.createElement('td');
-      // tableData1.id = receivedList.quality;
 
 
 
@@ -195,7 +183,7 @@ function getCustomerData() {
       tablerow.appendChild(tableData2);
       tablerow.appendChild(tableData3);
       tablerow.appendChild(tableData4);
-
+     
       receivedDable.appendChild(tablerow);
       totalPrice += (receivedList.price * receivedList.quality);
     }
@@ -212,23 +200,17 @@ function search() {
   let totalBooks = getBookList();
   let searchInput = document.getElementById('titleorAuthor').value;
   let selectedOption = document.getElementById('book').value;
-  
+
   console.log(searchInput, "searchInput");
   console.log(selectedOption, "selectedOption");
-  //  let book  = totalBooks.some(val => return val.bookName != selectedOption ) 
-  //   console.log(book ,"return value");
+ 
 
   let filteredList = totalBooks.filter(value => {
     return value.authorName == selectedOption || value.bookName == searchInput || value.authorName == searchInput
 
   });
-  // for (const iterator of filteredList) {
-  //   if(iterator.bookName != selectedOption ){
+
   ListOfFilterData = filteredList;
-  //   }
-  // }
-  // ListOfFilterData = filteredList;
-  // allItems.push(ListOfFilterData);
   console.log(ListOfFilterData, "filteredList");
   display();
   get();
@@ -242,14 +224,10 @@ function search() {
 
 function orderItems() {
   removeItem();
-  // for (const list of ListOfFilterData) {
-    // var x = document.getElementById("toaste");
-    // x.className = "show";
-    // setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 
   for (const list of allItems) {
     let inputValue = document.getElementById(list.bookName);
-    // objectOfOrders.push(inputValue)
+
     if (inputValue.checked) {
       let quantityValue = document.getElementById(list.qId).value;
       let name = document.getElementById('name').value;
@@ -271,7 +249,10 @@ function orderItems() {
 
 
   }
+
+
   display();
+
   getCustomerData();
   console.log(objectOfOrders, "orderObject");
 
@@ -289,8 +270,7 @@ function removeOldData() {
   count = 0;
 }
 
-function removeOldDataOfSearch(){
-
+function removeOldDataOfSearch() {
   for (const receivedList of totalList) {
     let tablerow = document.getElementById(receivedList.authorName);
     tablerow.remove();
